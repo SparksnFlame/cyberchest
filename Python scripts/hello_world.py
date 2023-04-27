d3803b18a0add0e3870642be28448a5e769f2195
Jose Vilela <jose.sergio.vilela@tecnico.ulisboa.pt>
2023-04-27 23:07:26 +0100

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Python scripts/hello_world.py     Basic python scripts

---
+++ Python scripts/hello_world.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#Print strings
+print("Hello World\n")
+print("\n")
+print('Hello World')
+
+#Do MATH
+print (50+50/10)
+print((50**2)+10) #print 50 squared + 10 = 2510
+print(50%6) #print remainder of division
+print(50/6) #print float
+print(50//6) #print integer part only
